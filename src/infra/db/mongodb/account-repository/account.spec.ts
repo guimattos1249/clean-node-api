cfe523cfc8f5a9db149c7f61d1c5abec9c255e97
Guilherme Mattos <guilhermevsmattos@outlook.com>
2021-04-26 19:01:34 -0300

    test: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,7 +11,7 @@ describe('NAccount Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
